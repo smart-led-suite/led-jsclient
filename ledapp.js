52f6e625dad95938fe47fb9885cbd74b255f1184
Sandesh <sansha98@gmail.com>
2016-12-25 15:13:04 +0100

    add some functionality

--- ledapp.js
+++ ledapp.js
@@ -1,13 +1,31 @@
 angular.module('ledapp', [])
-    .controller('MainCtrl', function ($scope) {
+    .controller('MainCtrl', function ($scope, $http) {
+      // get the profiles from JSON file
+      $http.get('config.json').then(function (response) {
+        $scope.profiles = response.data[0].profiles;
+        $scope.leds = response.data[1].leds;
+        console.log($scope.profiles);
+        console.log($scope.leds);
+      });
       $scope.blub = 'sdfkjafla';
+      this.activeProfile = 0;
+      // this.activeProfile = 0;
       this.blub = 'haddl';
-      this.profiles = [
-            {name: 'profile1', value: [30, 30]},
-            {name: 'profile2', value: [34, 27]}
-      ];
-      this.sendValue = function () {
+      if ($scope.profiles !== undefined) {
+        console.log($scope.profiles);
+      }
+      this.saveProfile = function () {
+        console.log($scope.profiles);
+      };
+      console.log($scope.profiles);
+      this.changeProfile = function (newProfile) {
+        console.log('blub');
+        this.activeProfile = newProfile;
+        console.log(this.activeProfile);
+      };
+      this.sendValue = function (newValue, index) {
+        console.log(newValue + ' index: ' + index);
+        $scope.profiles[this.activeProfile].value[index] = newValue;
         console.log('hello');
-            // console.log(this.room);
       };
     });
